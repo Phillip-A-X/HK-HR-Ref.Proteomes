9a9dcebdb319f3ddfe3e51edc4be3f7640df8eeb
Phillip-A-X <phhempel@students.uni-mainz.de>
2026-05-08 10:10:25 +0200

    Create constants.py
    dir for paths and definitions

---
+++ lib/constants.py
@@ -0,0 +1,13 @@
+#Constant Values
+
+UNIPROT_RELEASE = "2026_01"
+
+REFERENCE_PROTEOME_DIR = (
+    "/mnt/d/data/compressed/ftp.uniprot.org/pub/databases/uniprot/"
+    "release-2026_01/knowledgebase/reference_proteomes"
+)
+
+HUMAN_PROTEOME_ID = "UP000005640_9606"
+
+POLYX_IDENTICAL_AA = 8
+POLYX_WINDOW_SIZE = 10
