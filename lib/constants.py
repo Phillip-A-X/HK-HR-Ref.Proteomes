92817475c78aa1a1ab33d39112dfe2c1fe338168
Phillip-A-X <phhempel@students.uni-mainz.de>
2026-05-10 04:42:01 +0200

    Update constants.py

--- lib/constants.py
+++ lib/constants.py
@@ -1,15 +1,50 @@
 #Constant Values
+#Path
+
+from pathlib import Path
+
 
 UNIPROT_RELEASE = "2026_01"
 
 MOUNT_POINT = Path("/mnt/d/data/compressed")
 
 REFERENCE_PROTEOME_DIR = (
-    MOUNT_POINT + "/ftp.uniprot.org/pub/databases/uniprot/"
-    "release-2026_01/knowledgebase/reference_proteomes"
+    MOUNT_POINT /"ftp.uniprot.org"/"pub"/"databases"/"uniprot"/"
+    "release-2026_01"/"knowledgebase"/"reference_proteomes"
 )
 
+
+
+
+##Local directories
+
+DATA_DIR = Path("data")
+RAW_DATA_DIR = DATA_DIR / "raw"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+
+BIOMART_DIR = RAW_DATA_DIR / "biomart"
+
+RESULTS_DIR = Path("results")
+TABLES_DIR = RESULTS_DIR / "tables"
+FIGURES_DIR = RESULTS_DIR / "figures"
+
+
+
+
+#Human
+
 HUMAN_PROTEOME_ID = "UP000005640_9606"
+HUMAN_FASTA_FILE = REFERENCE_PROTEOME_DIR / f"{HUMAN_PROTEOME_ID}.fasta.gz"
+
+EISENBERG_HK_GENES_FILE = RAW_DATA_DIR / "eisenberg_hk_genes.tsv"
+
+BIOMART_EISENBERG_SWISSPROT_FILE = (
+    BIOMART_DIR / "mart_export100526.tsv"
+)
+
+
+
+
 
 POLYX_IDENTICAL_AA = 8
 POLYX_WINDOW_SIZE = 10
